9962e9bf5dc6e5f5c8133fb1b6356c3b54c97b33
Erbol <kerimov.erbol.00@gmail.com>
2024-07-18 22:48:27 +0500

    update

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,20 @@
+const theme_toggler = document.querySelector('.header__theme-toggle');
+const logoImg = document.querySelector('.header__theme-toggle-img');
+const heroScreen = document.querySelector('.hero__screenshot-img');
+
+theme_toggler.addEventListener('click', function(){ 
+    document.body.classList.toggle('dark_mode')
+    if(document.body.classList.contains('dark_mode')){
+        logoImg.src="img/theme/mode-dark.svg"
+    }else{
+        logoImg.src="img/theme/mode-light.svg"
+    }
+    
+});
+
+
 const coinObserver =new IntersectionObserver((entites)=>{
     entites.forEach((entity,index)=>{
-        console.log(entity)
-       
         if(entity.isIntersecting){
             setTimeout(()=>{
                 entity.target.classList.add('animate')
@@ -11,10 +24,17 @@ const coinObserver =new IntersectionObserver((entites)=>{
         }
     })
 })
+
+
 const coins=document.querySelectorAll(".currencies__img")
+console.log("1")
+console.log(coins)
 coins.forEach(element => {
+    console.log("2")
+    console.log(element)
     coinObserver.observe(element)
 });
+
 
 
 const cursorObserver = new IntersectionObserver((entries) => {
@@ -30,3 +50,26 @@ const cursorObserver = new IntersectionObserver((entries) => {
 });
 const coursor=document.querySelector(".commission__cursor")
 cursorObserver.observe(coursor)
+
+
+
+const exchangeObserver = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log("Element is intersecting");
+                document.querySelector(".exchage__first").classList.add('animate-exchane-1');
+                document.querySelector(".exchage__second").classList.add('animate-exchane-2');
+                document.querySelector(".exchage__third").classList.add('animate-exchane-3');
+            } else {
+                console.log("Element is not intersecting");
+                document.querySelector(".exchage__first").classList.remove('animate-exchane-1');
+                document.querySelector(".exchage__second").classList.remove('animate-exchane-2');
+                document.querySelector(".exchage__third").classList.remove('animate-exchane-3');
+            }
+        });
+    }
+);
+
+const exchangeDemo = document.querySelector('.exchage__wrapper');
+exchangeObserver.observe(exchangeDemo);
